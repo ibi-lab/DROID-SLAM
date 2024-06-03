2bdce69721907ec373f4df402ed0b836cb79e2c9
杉山治 <sugiyama@halo.local>
2024-06-03 09:41:11 +0900

    update visualization

--- droid_slam/visualization.py
+++ droid_slam/visualization.py
@@ -2,14 +2,11 @@ import torch
 import cv2
 import lietorch
 import droid_backends
-import time
-import argparse
 import numpy as np
 import open3d as o3d
 from lietorch import SE3
 import geom.projective_ops as pops
-from google.colab.patches import cv2_imshow
-import logging
+import matplotlib.pyplot as plt
 
 CAM_POINTS = np.array([
         [ 0,   0,   0],
@@ -37,7 +34,6 @@ def create_camera_actor(g, scale=0.05):
     camera_actor = o3d.geometry.LineSet(
         points=o3d.utility.Vector3dVector(scale * CAM_POINTS),
         lines=o3d.utility.Vector2iVector(CAM_LINES))
-
     color = (g * 1.0, 0.5 * (1-g), 0.9 * (1-g))
     camera_actor.paint_uniform_color(color)
     return camera_actor
@@ -50,120 +46,60 @@ def create_point_actor(points, colors):
 
 def droid_visualization(video, device="cuda:0"):
     torch.cuda.set_device(device)
-    droid_visualization.video = video
-    droid_visualization.cameras = {}
-    droid_visualization.points = {}
-    droid_visualization.warmup = 8
-    droid_visualization.scale = 1.0
-    droid_visualization.ix = 0
-    droid_visualization.filter_thresh = 0.005
+    video.cameras = {}
+    video.points = {}
+    warmup = 8
+    scale = 1.0
+    ix = 0
+    filter_thresh = 0.005
 
-    def increase_filter(vis):
-        droid_visualization.filter_thresh *= 2
-        with droid_visualization.video.get_lock():
-            droid_visualization.video.dirty[:droid_visualization.video.counter.value] = True
+    poses = video.poses.cpu()
+    disps = video.disps.cpu()
+    images = video.images.cpu()[:, [2,1,0], 3::8, 3::8].permute(0, 2, 3, 1) / 255.0
+    intrinsics = video.intrinsics[0].cpu()
 
-    def decrease_filter(vis):
-        droid_visualization.filter_thresh *= 0.5
-        with droid_visualization.video.get_lock():
-            droid_visualization.video.dirty[:droid_visualization.video.counter.value] = True
+    # OpenCV video writer setup
+    height, width = 540, 960
+    out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 10, (width, height))
 
-    def animation_callback(vis):
-        logging.error('start animation callback')
-        cam = vis.get_view_control().convert_to_pinhole_camera_parameters()
+    for t in range(video.counter.value):
+        pose = SE3(poses[t]).inv().matrix().cpu().numpy()
+        disp = disps[t]
+        image = images[t]
+        points = droid_backends.iproj(SE3(poses[t]).inv().data, disp, intrinsics).cpu()
 
-        with torch.no_grad():
-            with video.get_lock():
-                t = video.counter.value 
-                dirty_index, = torch.where(video.dirty.clone())
-                dirty_index = dirty_index
+        count = droid_backends.depth_filter(
+            poses, disps, intrinsics, torch.tensor([t]), torch.tensor([filter_thresh]))
 
-            if len(dirty_index) == 0:
-                return
+        count = count.cpu()
+        mask = ((count >= 2) & (disp > .5 * disp.mean(dim=[1, 2], keepdim=True))).reshape(-1)
+        pts = points.reshape(-1, 3)[mask].numpy()
+        clr = image.reshape(-1, 3)[mask].numpy()
 
-            video.dirty[dirty_index] = False
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(visible=False)
+        cam_actor = create_camera_actor(True)
+        cam_actor.transform(pose)
+        vis.add_geometry(cam_actor)
 
-            poses = torch.index_select(video.poses, 0, dirty_index)
-            disps = torch.index_select(video.disps, 0, dirty_index)
-            Ps = SE3(poses).inv().matrix().cpu().numpy()
+        point_actor = create_point_actor(pts, clr)
+        vis.add_geometry(point_actor)
 
-            images = torch.index_select(video.images, 0, dirty_index)
-            images = images.cpu()[:,[2,1,0],3::8,3::8].permute(0,2,3,1) / 255.0
-            points = droid_backends.iproj(SE3(poses).inv().data, disps, video.intrinsics[0]).cpu()
+        vis.poll_events()
+        vis.update_renderer()
 
-            thresh = droid_visualization.filter_thresh * torch.ones_like(disps.mean(dim=[1,2]))
-            count = droid_backends.depth_filter(
-                video.poses, video.disps, video.intrinsics[0], dirty_index, thresh)
+        img = vis.capture_screen_float_buffer(False)
+        vis.destroy_window()
 
-            count = count.cpu()
-            disps = disps.cpu()
-            masks = ((count >= 2) & (disps > .5*disps.mean(dim=[1,2], keepdim=True)))
-            
-            for i in range(len(dirty_index)):
-                pose = Ps[i]
-                ix = dirty_index[i].item()
+        img = (np.asarray(img) * 255).astype(np.uint8)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-                if ix in droid_visualization.cameras:
-                    vis.remove_geometry(droid_visualization.cameras[ix])
-                    del droid_visualization.cameras[ix]
+        # Write frame to video
+        out.write(img)
 
-                if ix in droid_visualization.points:
-                    vis.remove_geometry(droid_visualization.points[ix])
-                    del droid_visualization.points[ix]
-
-                cam_actor = create_camera_actor(True)
-                cam_actor.transform(pose)
-                vis.add_geometry(cam_actor)
-                droid_visualization.cameras[ix] = cam_actor
-
-                mask = masks[i].reshape(-1)
-                pts = points[i].reshape(-1, 3)[mask].cpu().numpy()
-                clr = images[i].reshape(-1, 3)[mask].cpu().numpy()
-                
-                point_actor = create_point_actor(pts, clr)
-                vis.add_geometry(point_actor)
-                droid_visualization.points[ix] = point_actor
-
-            # if len(droid_visualization.cameras) >= droid_visualization.warmup:
-            cam = vis.get_view_control().convert_from_pinhole_camera_parameters(cam)
-
-            droid_visualization.ix += 1
-            vis.poll_events()
-            vis.update_renderer()
-
-            logging.error('captuing open3d')
-            vis.capture_screen_image(f"/content/frames/frame_{droid_visualization.ix:05d}.png")
-
-    # vis = o3d.visualization.VisualizerWithKeyCallback()
-    vis = o3d.visualization.Visualizer()
-    vis.register_animation_callback(animation_callback)
-    # vis.register_key_callback(ord("S"), increase_filter)
-    # vis.register_key_callback(ord("A"), decrease_filter)
-
-    # vis.create_window(height=540, width=960)
-    # vis.get_render_option().load_from_json("misc/renderoption.json")
-
-    logging.error('start running open3d visualizer')
-    vis.run()
-    logging.error('finish running open3d visualizer')
-    # vis.destroy_window()
-
-    logging.error('start rendering images')
-    images = []
-    for i in range(1, droid_visualization.ix + 1):
-        img = cv2.imread(f"/content/frames/frame_{i:05d}.png")
-        images.append(img)
-
-    height, width, layers = images[0].shape
-    size = (width, height)
-    out = cv2.VideoWriter('/content/point_cloud_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-
-    for image in images:
-        out.write(image)
     out.release()
+    print("Video saved as output.mp4")
 
-    return '/content/point_cloud_video.avi'
-
-# Usage
-# video is assumed to be an instance with necessary attributes
+# 例としての呼び出し方法
+# video = ...  # DROIDシステムから得られたビデオデータ
 # droid_visualization(video)
